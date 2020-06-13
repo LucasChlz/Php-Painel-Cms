e3e0afc11493073cb4eab19f36fcc84980dceda8
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2020-06-13 17:29:29 -0300

    front-end home page (image picks up on pixebay website)

--- app/views/home.php
+++ app/views/home.php
@@ -1,1 +1,68 @@
-<H2>Home Page</H2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="<?= APP_PATH; ?>/style/style.css"/>
+  <title>Site</title>
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="logo"><i class="large material-icons">terrain</i></div>
+    <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+    </ul>
+    </div><!--container-->
+    <div class="clear"></div>
+</header>
+<section id="about" class="container-about">
+    <div class="background">
+        <div class="bg-color">
+            <div class="container">
+                <div class="txt">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, architecto amet. Inventore quae nihil necessitatibus. Distinctio quod, quisquam rerum sunt iure doloribus commodi hic consequatur dolorem fuga velit sit vero.</p>
+                    <br>
+                    <br>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, architecto amet. Inventore quae nihil necessitatibus. Distinctio quod, quisquam rerum sunt iure doloribus commodi hic consequatur dolorem fuga velit sit vero.</p>
+                </div><!--txt-->
+            </div><!--container-->
+        </div><!--bg-color-->
+    </div><!--background-->
+</section><!--container-about-->
+
+<section id="services" class="container-service">
+    <h2>Services</h2>
+    <div class="container flex">
+        <div class="box-service">
+            <div class="icon"><i class="large material-icons">aspect_ratio</i></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis vitae mollitia ab quos perferendis, animi et minus necessitatibus recusandae aperiam rerum ex possimus maxime fugit, cupiditate nobis magnam tempore eius!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis vitae mollitia ab quos perferendis, animi et minus necessitatibus recusandae aperiam</p>
+        </div><!--box-service-->
+
+        <div class="box-service">
+            <div class="icon"><i class="large material-icons">aspect_ratio</i></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis vitae mollitia ab quos perferendis, animi et minus necessitatibus recusandae aperiam rerum ex possimus maxime fugit, cupiditate nobis magnam tempore eius!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis vitae mollitia ab quos perferendis, animi et minus necessitatibus recusandae aperiam</p>
+        </div><!--box-service-->
+
+        <div class="box-service">
+            <div class="icon"><i class="large material-icons">aspect_ratio</i></div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis vitae mollitia ab quos perferendis, animi et minus necessitatibus recusandae aperiam rerum ex possimus maxime fugit, cupiditate nobis magnam tempore eius!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis vitae mollitia ab quos perferendis, animi et minus necessitatibus recusandae aperiam</p>
+        </div><!--box-service-->
+
+    </div><!--container-->
+</section><!--container--service-->
+
+<footer>
+    <div class="container">
+        <div class="footer">
+            <h2>FOOTER</h2>
+        </div><!--footer-->
+    </div><!--container-->
+</footer>
+</body>
+</html>
